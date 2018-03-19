4a30c0858a1b311ec81366c45e7de828039e064e
manuelvilla <manuelvillasenor.sa@gmail.com>
2018-03-18 23:46:20 -0600

    Primeros cambios

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
-var calculadora = ''
-
-function botonTamanio (){
-	
+function presionarBoton (elemento){
+	elemento.style.width="90%";
+	elemento.style.height="90%";
 }
+
+var calculadora ={
+	init: function(){
+	
+	}
+}
+
+//teclas width:23% a 21% y height:18% a 17%
+
