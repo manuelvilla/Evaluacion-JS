68ada29d9b9cf00f0f548fdf1e5ebc39908cb37e
manuelvilla <manuelvillasenor.sa@gmail.com>
2018-03-20 19:52:02 -0600

    Declaración de variables
    Botones convertidos a variables y su invocación

--- js/app.js
+++ js/app.js
@@ -1,13 +1,34 @@
-function presionarBoton (elemento){
-	elemento.style.width="90%";
-	elemento.style.height="90%";
-}
+//Declaración de variables más importantes
+var num1 = ; //Número 1
+var num2 = ; //Número 2
+var operacion ; //Operaciones a realizar
 
-var calculadora ={
-	init: function(){
+var Calculadora = { //Objeto Calculadora
 	
+	init: function(){//Método de inicialización
+		//Declaración de variables botones
+		var onC = document.getElementById('on');
+		var masMenos = document.getElementById('sign');
+		var division = document.getElementById('dividido');
+		var multiplicacion = document.getElementById('por');
+		var resta = document.getElementById('menos');
+		var suma = document.getElementById('mas');
+		var igual = document.getElementById('igual');
+		var punto = document.getElementById('punto');
+		var cero = document.getElementById('0');
+		var uno = document.getElementById('1');
+		var dos = document.getElementById('2');
+		var tres = document.getElementById('3');
+		var cuatro = document.getElementById('4');
+		var cinco = document.getElementById('5');
+		var seis = document.getElementById('6');
+		var siete = document.getElementById('7');
+		var ocho = document.getElementById('8');
+		var nueve = document.getElementById('9');
 	}
+
 }
 
-//teclas width:23% a 21% y height:18% a 17%
 
+document.getElementsByClassName('teclado')[0].getElementsByTagName('img').onclick = presionarBoton;
+//teclas width:23% a 21% y height:18% a 17%
